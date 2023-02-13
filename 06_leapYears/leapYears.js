b6eaf95867fce9cfdf1b70733a9e392c879410b7
ikojun00 <ikojun00@fesb.hr>
2023-02-13 12:45:10 +0100

    Task completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+//if divisible by 4 => true
+//if divisible by 100 => false
+//if divisible by 400 => true
 
+const leapYears = function(year) {
+    if(year%400 === 0)
+        return true;
+    else if(year%100 === 0)
+        return false;
+    else if(year%4 === 0)
+        return true;
+    else
+        return false;
 };
 
 // Do not edit below this line
