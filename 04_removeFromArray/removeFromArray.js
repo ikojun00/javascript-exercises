286c2dd3fd606ea8a92fcb972a3a6b5425c98d07
ikojun00 <ikojun00@fesb.hr>
2023-02-13 12:04:24 +0100

    Works when i sort arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) 
+{
+    theArgs = theArgs.sort();
+    for (let i = 0; i < array.length; i++) 
+    {
+        for (let j = 0; j < theArgs.length; j++)
+        {
+            if (array[i] === theArgs[j]) 
+            {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
