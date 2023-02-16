7af0c7bccf28bdc9e84767bd80a791a4046d6f77
ikojun00 <ikojun00@fesb.hr>
2023-02-16 10:32:08 +0100

    Task completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let sum = 0;
+	a.forEach(element => sum = sum + parseInt(element));
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let multiply = 1;
+	a.forEach(element => multiply = multiply * parseInt(element));
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let power = 1;
+  for(let i=0; i<b; i++)
+    power = power * a;
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorial = 1;
+  for(let i=1; i<=a; i++)
+    factorial = factorial * i;
+  return factorial;
 };
 
 // Do not edit below this line
