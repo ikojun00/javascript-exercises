5d0fcc09af2b199da2c4cf48cfbc5be8a4e4e234
ikojun00 <ikojun00@fesb.hr>
2023-02-16 11:25:01 +0100

    Task completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    let b = "";
+    a = a.toLowerCase();
+    for(let i=0; i<a.length; i++)
+    {
+        if(a[i]<'a' || a[i]>'z')
+            a=a.replace(a[i], '');
+    }
+    a=a.replaceAll(' ', '');
+    for(let i=a.length-1; i>=0; i--)
+        b=b.concat(a[i]);
+    if(a===b)
+        return true;
+    else
+        return false;
 };
 
 // Do not edit below this line
