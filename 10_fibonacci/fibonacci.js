e39caacf9bbf98f084c57f3f52319b45aafe64a9
ikojun00 <ikojun00@fesb.hr>
2023-02-16 12:32:27 +0100

    Task completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let a=[];
+    num = parseInt(num);
+    if(num === 1 || num === 2)
+        return 1;
+    else if(num === 0)
+        return 0;
+    else if(num < 0)
+        return "OOPS";
+    else
+    {
+        let n1 = 1, n2 = 1;
+        for (let i = 2; i < num; i++) {
+            nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm;
+        }
+    }
+    return nextTerm;
 };
 
 // Do not edit below this line
